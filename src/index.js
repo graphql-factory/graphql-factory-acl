72bf6d44b12108b93f6bbe07c519dd84a00548c6
Branden Horiuchi <bhoriuchi@gmail.com>
2017-10-07 01:31:14 -0700

    re-worked error calculation
    now a list of requested paths is compared to the permissions

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ const ADMIN_ROLE = 'admin'
 
 /**
  * Creates a list of resource paths the user is requesting access to
- * which includes not paths, query paths, and args paths
+ * which includes not paths, selection paths, and args paths
  * @param info
+ * @param args
+ * @param schemaName
  * @returns {[null]}
  */
 function buildResources (info, args, schemaName) {
@@ -48,11 +50,11 @@ function buildResources (info, args, schemaName) {
     paths.push(`${schemaName}.*`)
     paths.push(`${schemaName}.${op}.*`)
     paths.push(`${schemaName}.${op}.${field}.*`)
-    paths.push(`${schemaName}.${op}.${field}.query.*`)
+    paths.push(`${schemaName}.${op}.${field}.selection.*`)
     paths.push(`${schemaName}.${op}.${field}.args.*`)
 
-    traverseSelections(node.selectionSet, notBase.slice().concat('query'), paths)
-    traverseSelections(node.selectionSet, base.slice().concat('query'), paths)
+    traverseSelections(node.selectionSet, notBase.slice().concat('selection'), paths)
+    traverseSelections(node.selectionSet, base.slice().concat('selection'), paths)
     if (args) {
       traverseArgs(args, notBase.slice().concat('args'), paths)
       traverseArgs(args, base.slice().concat('args'), paths)
@@ -63,71 +65,40 @@ function buildResources (info, args, schemaName) {
 }
 
 /**
- * Creates an object that can be used to filter the selection set
+ * creates paths for the requested selections and args
  * @param info
- * @param list
- * @param requiredPerms
+ * @param args
  * @param basePath
- */
-function buildKeepObject (info, list, requiredPerms, basePath) {
-  return _.reduce(list, (obj, perms, path) => {
-    // if the required permission is not present, move on
-    if (!_.intersection(perms, requiredPerms).length) return obj
-
-    // remove the basepath since we are only interested in the requested fields
-    path = path.replace(new RegExp(`^${basePath}.`), '')
-
-    // check for inherited path
-    if (path.match(/\.\*$/)) {
-      _.set(obj, path.replace(/\.\*$/, ''), true)
-      return obj
-    }
-
-    let o = obj
-    let fields = _.toPath(path)
-
-    // look for already set paths so that they are not overwritten
-    for (let i = 0; i < fields.length; i++) {
-      if (o[fields[i]] === true) return obj
-      else o = o[fields[i]]
-      if (o === undefined) break
-    }
-
-    // set the path if it has not been set
-    _.set(obj, path, true)
-
-    return obj
-  }, {})
-}
-
-/**
- * Builds a list of fields that the user does not have access to
- * @param info
- * @param keep
- * @param basePath
- * @param errors
  * @returns {Array}
  */
-function buildErrorList (info, keep, basePath, errors = []) {
+function createRequestPaths (info, args, basePath) {
+  const paths = []
   let traverseSelections = (selectionSet, path) => {
     if (!_.get(selectionSet, 'selections')) return
     _.forEach(selectionSet.selections, sel => {
       let p = path.slice()
       p.push(sel.name.value)
-      let keepValue = _.get(keep, p)
-      if (!keepValue) {
-        errors.push(`${basePath}.${p.join('.')}`)
-      } else if (keepValue !== true) {
-        traverseSelections(sel.selectionSet, p)
-      }
+      paths.push(p.join('.'))
+      traverseSelections(sel.selectionSet, p)
+    })
+  }
+
+  let traverseArgs = (a, path) => {
+    if (!_.isObject(a) || _.isArray(a)) return
+    _.forEach(a, (val, key) => {
+      let p = path.slice()
+      p.push(key)
+      paths.push(p.join('.'))
+      traverseArgs(val, p)
     })
   }
 
   _.forEach(info.fieldNodes || info.fieldASTs, node => {
-    traverseSelections(node.selectionSet, [])
+    traverseSelections(node.selectionSet, [basePath, 'selection'])
   })
 
-  return errors
+  traverseArgs(args, [basePath, 'args'])
+  return paths
 }
 
 /**
@@ -201,6 +172,7 @@ export default class GraphQLFactoryACLPlugin {
     // add the acl middleware
     definition.beforeResolve(function (resolverArgs, next) {
       try {
+        const errors = []
         const GraphQLError = this.graphql.GraphQLError
         let { args, info } = resolverArgs
         let op = _.get(info, 'operation.operation')
@@ -220,7 +192,9 @@ export default class GraphQLFactoryACLPlugin {
 
         // check for jwt in the rootValue
         let token = _.get(info, 'rootValue.jwt')
-        if (!token || !_.isString(token)) return next(new Error('ACLError: No jwt was provided in the rootValue of the request'))
+        if (!token || !_.isString(token)) {
+          return next(new Error('ACLError: No jwt was provided in the rootValue of the request'))
+        }
 
         return jwt.verify(token, secret, (err, decoded) => {
           if (err) return next(err)
@@ -228,50 +202,42 @@ export default class GraphQLFactoryACLPlugin {
           if (!userId) return next(new Error('ACLError: No userId found in the provided jwt'))
 
           const resources = buildResources(info, args, schemaName)
+          const reqPaths = createRequestPaths(info, args, basePath)
 
           return acl.allowedPermissions(userId, resources, (err, list) => {
             if (err) return next(err)
-
-            console.log(list)
-
-            // check for all access
-            if (_.intersection(_.get(list, '*', []), requiredPerms).length) {
-              return next()
-            }
 
             // check that there are some permissions, if not then the userid has no access
             if (!Object.keys(_.pickBy(list, perm => perm.length > 0)).length) {
               return next(new Error(`User "${userId}" has no permissions on "${basePath}"`))
             }
 
-            // get cumulative base permissions for query an args
-            const baseQueryPerms = _.reduce([schemaName, op, info.fieldName, 'query'], (accum, cur) => {
-              accum.path.push(cur)
-              accum.perms = _.union(accum.perms, _.get(list, accum.path.concat('*').join('.'), []))
-              return accum
-            }, { path: [], perms: [] }).perms
-            const baseArgsPerms = _.reduce([schemaName, op, info.fieldName, 'args'], (accum, cur) => {
-              accum.path.push(cur)
-              accum.perms = _.union(accum.perms, _.get(list, accum.path.concat('*').join('.'), []))
-              return accum
-            }, { path: [], perms: [] }).perms
+            // check if the not list contains the argument
+            reqPaths.map(v => `!${v}`).forEach(p => {
+              const perms = _.union(_.get(list, p, []), _.get(list, `${p}.*`, []))
+              if (perms.length) errors.push(p.replace(/^!/, ''))
+            })
 
-            // check for the * resource and permission, if found then authorize
-            if (_.intersection(baseQueryPerms, requiredPerms).length) {
-              if (!_.keys(args).length || _.intersection(baseArgsPerms, requiredPerms).length) {
-                return next()
+            // check each field
+            reqPaths.forEach(v => {
+              // check all permutations of privileges on the current path
+              // and reduce those perms to a list that can be evaluated
+              // against the required permissions
+              let perms = _.reduce(v.split('.'), (accum, part) => {
+                accum.path.push(part)
+                const perm = _.union(
+                  _.get(list, '*'),
+                  _.get(list, accum.path.join('.')),
+                  _.get(list, accum.path.concat('*').join('.'))
+                )
+                accum.perm = _.union(accum.perm, perm)
+                return accum
+              }, { path: [], perm: [] }).perm
+
+              if (!_.intersection(perms, requiredPerms).length) {
+                errors.push(v)
               }
-            }
-
-            // TODO: re-work the keep and error evauluators to handle the new path structure and not cases
-
-            // build an object that represents the fields to keep
-            let keep = buildKeepObject(info, list, requiredPerms, basePath)
-
-            // next build an error list
-            let errors = _.keys(keep).length
-              ? buildErrorList(info, keep, basePath)
-              : [basePath]
+            })
 
             // return the response
             return errors.length
